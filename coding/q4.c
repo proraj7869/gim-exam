dfff1e7d078abdd43bfee2811653c10cbed1e2d9
Raj Gandhi <rjgandhi159@gmail.com>
2025-01-20 12:35:41 +0530

    Add files via upload

--- coding/q4.c
+++ coding/q4.c
@@ -9,17 +9,22 @@ int main()
     scanf("%c", &ch);
 
     
-    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
         if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U') {
             printf("%c is a vowel.\n", ch);
-        } else {
+        } 
+        else 
+        {
             printf("%c is a consonant.\n", ch);
         }
-    } else {
+    }
+    else 
+    {
         printf("Invalid input! Please enter an alphabet.\n");
     }
 
-    
+ return 0;
 }
 
 
